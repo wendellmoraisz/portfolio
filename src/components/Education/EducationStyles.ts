0c2f5f2ae76c27375c22c30b5e7d6995686834d5
wendellmoraisz <wendellmorais3319@gmail.com>
2023-12-20 16:50:10 -0300

    fix: education card overriding footer

--- src/components/Education/EducationStyles.ts
+++ src/components/Education/EducationStyles.ts
@@ -7,10 +7,7 @@ export const Container = styled.div`
     position: relative;
     z-index: 1;
     align-items: center;
-    padding: 0px 0px 60px 0px;
-    @media (max-width: 960px) {
-        padding: 0px;
-    }
+    padding: 0px 20px 60px 0px;
 `;
 
 export const Wrapper = styled.div`
@@ -20,36 +17,33 @@ export const Wrapper = styled.div`
     align-items: center;
     flex-direction: column;
     width: 100%;
-    max-width: 1350px;
-    padding: 40px 0px 0px 0px;
+    max-width: 1200px;
     gap: 12px;
-    @media (max-width: 960px) {
-        flex-direction: column;
+    
+    @media (max-width: 768px) {
+        margin-bottom: 60px;
     }
 `;
 
 export const Title = styled.div`
-font-size: 42px;
-text-align: center;
-font-weight: 600;
-margin-top: 20px;
+  font-size: 2.5rem;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 20px;
   color: ${({ theme }) => theme.text_primary};
   @media (max-width: 768px) {
       margin-top: 12px;
-      font-size: 32px;
+      font-size: 2rem;
   }
 `;
 
 export const TimelineSection = styled.div`
     width: 100%;
-    max-width: 1000px;
+    max-width: 1200px;
     margin-top: 10px;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     gap: 12px;
-    @media (max-width: 660px) {
-        align-items: end;
-    }
 `;
