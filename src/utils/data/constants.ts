e4dbbb4ff467833bbc7529df658cfbf07686a6d0
wendellmoraisz <wendellmorais3319@gmail.com>
2024-01-03 10:37:06 -0300

    raw: update git and postman logo url

--- src/utils/data/constants.ts
+++ src/utils/data/constants.ts
@@ -84,7 +84,7 @@ export const SkillsSections: SkillSection[] = [
       {
         name: "Git",
         image:
-          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
       },
       {
         name: "GitHub",
@@ -114,7 +114,7 @@ export const SkillsSections: SkillSection[] = [
       {
         name: "Postman",
         image:
-          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+          "https://cdn.worldvectorlogo.com/logos/postman.svg",
       },
     ],
   },
