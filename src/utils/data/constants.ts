b7fc1093fc7c57e65135939a4ea4db95c2663d34
Wendell <wendellmorais.dev@gmail.com>
2024-09-03 11:50:31 -0300

    raw: update bio

--- src/utils/data/constants.ts
+++ src/utils/data/constants.ts
@@ -7,9 +7,9 @@ import SkillSection from "../../interfaces/SkillSection";
 export const Bio: ProfileBio = {
   name: "Wendell Morais",
   role: "Desenvolvedor Fullstack",
-  description: "Desenvolvedor de software há mais de um ano, com experiência em desenvolvimento Fullstack. Amante de tecnologia desde pequeno, apaixonado por desenvolver ideias e transformá-las em realidade com a programação.",
+  description: "Desenvolvedor de software há mais de dois anos, com experiência em desenvolvimento Fullstack. Amante de tecnologia desde pequeno, apaixonado por desenvolver ideias e transformá-las em realidade com a programação.",
   github: "https://github.com/wendellmoraisz",
-  resume: "https://drive.google.com/file/d/1iNMBhmWH1Ijhd1rI3gusJ39dh-xCE1BG/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1TjqWoTwDluvAcxo1XkyaoNlrYiVrI5LN/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/wendell-morais/",
 };
 
